7fd096c78da8991deace78fbbe443060a4815f09
LoganXav <sogbesansegun22@gmail.com>
2023-06-08 18:03:24 +0100

    commit message

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { lazy, Suspense } from "react";
 import {
-  createBrowserRouter,
-  RouterProvider,
-  Outlet,
-  Navigate,
+  Routes,
+  Route,
+  BrowserRouter,
 } from "react-router-dom";
-import CircularProgress from '@mui/material/CircularProgress';
+import CircularProgress from "@mui/material/CircularProgress";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
@@ -13,53 +12,31 @@ import Home from "./pages/Home";
 const Gallery = lazy(() => import("./pages/Gallery"));
 import "./sass/main.scss";
 
-const Layout = () => {
-  return (
-    <div className="app">
-      <Navbar />
-      <Outlet />
-      <Footer />
-    </div>
-  );
-};
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "*",
-        element: <Home />,
-      },
-      {
-        path: "/gallery",
-        element: (
-          <Suspense fallback={<div className="circle"><CircularProgress /></div>}>
-            <Gallery />
-          </Suspense>
-        ),
-      },
-    ],
-  },
-]);
-
 function App() {
   return (
-    <div>
-      <RouterProvider router={router} />
-    </div>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route
+            path="/gallery"
+            element={
+              <Suspense
+                fallback={
+                  <div className="circle">
+                    <CircularProgress />
+                  </div>
+                }
+              >
+                <Gallery />
+              </Suspense>
+            }
+          ></Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
   );
 }
-
 export default App;
-
-{/* <Routes>
-  <Route path= "/" element = {<Layout />}></Route>
-</Routes> */}
-
-
